c1d28a90dcca245c5a54398637624bd434e33f47
Ivan-Gusev-1993 <ivangusev1993@gmail.com>
2023-06-14 20:07:50 +0300

    four week ToDoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import TodoList from "./TodoList";
+import {v1} from "uuid";
+
+export type TaskType = {
+    id: string
+    title: string
+    isDone: boolean
+}
+
+export type ButtonFilterType = 'all' | 'active' | 'completed'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [tasks, setTasks] = useState([
+            {id: v1(), title: "HTML & CSS", isDone: true},
+            {id: v1(), title: "CSS & SCSS", isDone: true},
+            {id: v1(), title: "ES6/TS", isDone: false},
+        ]
+    )
+
+    let [filterValue, setFilterValue] = useState<ButtonFilterType>('all')
+
+    const changeIsDone = (newId: string, newIsDone: boolean) => {
+        setTasks(tasks.map(el => el.id === newId ? {...el, isDone: newIsDone} : el))
+    }
+
+    const removeTask = (taskID: string) => {
+        setTasks(tasks.filter((el) => el.id !== taskID))
+    }
+
+    const addTask = (newTitle: string) => {
+        const newTask = {id: v1(), title: newTitle, isDone: false}
+        const newObj = [newTask, ...tasks]
+        setTasks(newObj)
+    }
+
+    let myButtonFilter = (buttonName: ButtonFilterType) => {
+        setFilterValue(buttonName)
+    }
+
+    let selectButtonValue = tasks;
+    if (filterValue === 'active') {
+        selectButtonValue = tasks.filter(el => !el.isDone)
+    }
+    if (filterValue === 'completed') {
+        selectButtonValue = tasks.filter(el => el.isDone)
+    }
+
+    return (
+        <div className="App">
+            <TodoList
+                title={'What to learn'}
+                task={selectButtonValue}
+                removeTask={removeTask}
+                myButtonFilter={myButtonFilter}
+                addTask={addTask}
+                changeIsDone={changeIsDone}
+            />
+        </div>
+    )
 }
 
 export default App;
