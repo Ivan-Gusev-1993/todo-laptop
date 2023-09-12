29b26587312a8f8fd61705ba7509b4d3dffbc275
Ivan-Gusev-1993 <ivangusev1993@gmail.com>
2023-09-12 19:14:53 +0300

    five week ToDoList

---
+++ src/EditableSpan.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type EditableSpanPropsType = {
+
+}
+
+export const EditableSpan = (props: EditableSpanPropsType) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
