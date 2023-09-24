b6388140abc7a8e00e79d370df08e87b2e734b56
Ivan-Gusev-1993 <ivangusev1993@gmail.com>
2023-09-24 07:20:52 +0300

    six week ToDoList

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -2,16 +2,16 @@ import React, {ChangeEvent, useState} from 'react';
 
 type EditableSpanPropsType = {
     oldTitle: string
-    callBack:(updateTitle:string)=>void
+    callBack: (updateTitle: string) => void
 }
 
 export const EditableSpan = (props: EditableSpanPropsType) => {
-    const [updateTitle, setUpdateTitle]=useState(props.oldTitle)
+    const [updateTitle, setUpdateTitle] = useState(props.oldTitle)
     const [edit, setEdit] = useState(false)
 
     const onDoubleClickHandler = () => {
         setEdit(!edit)
-        if(edit){
+        if (edit) {
             addTask()
         }
     }
@@ -21,7 +21,7 @@ export const EditableSpan = (props: EditableSpanPropsType) => {
     }
 
     const addTask = () => {
-    props.callBack(updateTitle)
+        props.callBack(updateTitle)
     }
 
     return (
