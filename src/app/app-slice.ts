981c4242252ce0f4978137af7d5980d951cf8cf1
Ivan Gusev <ivangusev1993@gmail.com>
2025-06-16 22:35:32 +0300

    used extraReducer for tasks reducer

---
+++ src/app/app-slice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+export const appSlice = createSlice({
+  name: "app",
+  initialState: {
+    themeMode: "light" as ThemeMode,
+  },
+  reducers: (create) => {
+    return {
+      changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
+        state.themeMode = action.payload.themeMode
+      }),
+    }
+  },
+})
+
+export type ThemeMode = "dark" | "light"
+
+export const appReducer = appSlice.reducer
+export const { changeThemeModeAC } = appSlice.actions
