4be3efc2af2fa2ad9c8c1490c960fa1b2174c97f
Ivan Gusev <ivangusev1993@gmail.com>
2025-08-29 11:35:02 +0300

    created loading

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,17 +6,25 @@ import { Routing } from "@/common/routing"
 import { getTheme } from "@/common/theme"
 import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { meTC } from "@/features/auth/model/auth-slice.ts"
 
 export const App = () => {
+  const [isInit, setIsInit] = useState(false)
+
   const themeMode = useAppSelector(selectThemeMode)
   const dispatch = useAppDispatch()
 
   const theme = getTheme(themeMode)
   useEffect(() => {
-    dispatch(meTC())
+    dispatch(meTC()).then(() => {
+      setIsInit(true)
+    })
   }, [])
+
+  if (!isInit) {
+    return <h2>Loading...</h2>
+  }
   return (
     <ThemeProvider theme={theme}>
       <div className={"app"}>
