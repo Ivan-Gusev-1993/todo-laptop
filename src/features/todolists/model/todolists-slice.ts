a2bb70d45042ed819ddbc85e2b66f52f988f032e
Ivan Gusev <ivangusev1993@gmail.com>
2025-09-04 18:01:37 +0300

    created clearDataAC

--- src/features/todolists/model/todolists-slice.ts
+++ src/features/todolists/model/todolists-slice.ts
@@ -4,6 +4,7 @@ import type { RequestStatus } from "@/common/types"
 import { createAppSlice, handleServerAppError, handleServerNetworkError } from "@/common/utils"
 import { todolistsApi } from "@/features/todolists/api/todolistsApi"
 import { type Todolist, todolistSchema } from "@/features/todolists/api/todolistsApi.types"
+import { clearDataAC } from "@/common/actions"
 
 export const todolistsSlice = createAppSlice({
   name: "todolists",
@@ -122,6 +123,18 @@ export const todolistsSlice = createAppSlice({
       }
     }),
   }),
+  extraReducers: (builder) => {
+    builder
+      .addCase(deleteTodolistTC.fulfilled, (state, action) => {
+        const index = state.findIndex((todolist) => todolist.id === action.payload.id)
+        if (index !== -1) {
+          state.splice(index, 1)
+        }
+      })
+      .addCase(clearDataAC, (_state) => {
+        return []
+      })
+  },
 })
 
 export const { selectTodolists } = todolistsSlice.selectors
