05b75edb78f90fac293b2c279acdd50c1d13182b
Ivan Gusev <ivangusev1993@gmail.com>
2025-09-12 20:16:06 +0300

    changed useEffect in App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,28 +4,30 @@ import { ErrorSnackbar, Header } from "@/common/components"
 import { useAppDispatch, useAppSelector } from "@/common/hooks"
 import { Routing } from "@/common/routing"
 import { getTheme } from "@/common/theme"
-import { initializeAppTC } from "@/features/auth/model/auth-slice"
 import CircularProgress from "@mui/material/CircularProgress"
 import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
 import { useEffect, useState } from "react"
 import styles from "./App.module.css"
 import { useMeQuery } from "@/features/auth/api/authApi.ts"
+import { ResultCode } from "@/common/enums"
+import { setIsLoggedInAC } from "@/features/auth/model/auth-slice.ts"
 
 export const App = () => {
   const [isInitialized, setIsInitialized] = useState(false)
   const themeMode = useAppSelector(selectThemeMode)
 
-  const { data } = useMeQuery()
+  const { data, isLoading } = useMeQuery()
 
   const dispatch = useAppDispatch()
 
   const theme = getTheme(themeMode)
 
   useEffect(() => {
-    dispatch(initializeAppTC()).finally(() => {
-      setIsInitialized(true)
-    })
+    if (isLoading) setIsInitialized(true)
+    if (data?.resultCode === ResultCode.Success) {
+      dispatch(setIsLoggedInAC({ isLoggedIn: true }))
+    }
   }, [])
 
   if (!isInitialized) {
