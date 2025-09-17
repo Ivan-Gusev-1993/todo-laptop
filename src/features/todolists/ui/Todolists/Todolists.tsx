b7f71957ad001e3ec1f1072c3f9fe5519074ce7d
Ivan Gusev <ivangusev1993@gmail.com>
2025-09-17 18:51:14 +0300

    created TodolistSkeleton component

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -2,12 +2,22 @@ import { useGetTodolistsQuery } from "@/features/todolists/api/todolistsApi"
 import Grid from "@mui/material/Grid"
 import Paper from "@mui/material/Paper"
 import { TodolistItem } from "./TodolistItem/TodolistItem"
+import { Skeleton } from "@mui/material"
 
 export const Todolists = () => {
   const { data: todolists, isLoading, isFetching, status } = useGetTodolistsQuery()
 
   return (
     <>
+      <div>
+        <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
+
+        {/* For other variants, adjust the size with `width` and `height` */}
+        <Skeleton variant="circular" width={40} height={40} />
+        <Skeleton variant="rectangular" width={210} height={60} />
+        <Skeleton variant="rounded" width={210} height={60} />
+      </div>
+
       {todolists?.map((todolist) => (
         <Grid key={todolist.id}>
           <Paper sx={{ p: "0 20px 20px 20px" }}>
